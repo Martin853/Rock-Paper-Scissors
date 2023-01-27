c74796043b37e5c224c3a219f7e735c3856b12c2
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-01-27 20:25:38 +0100

    Created playRound()

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let answer;
-const items = ["Rock", "Paper", "Scissors"];
+const items = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
   return items[Math.floor(Math.random() * items.length)];
@@ -10,3 +10,94 @@ function playerSelection() {
   ans = ans.toLowerCase();
   return ans;
 }
+
+function playRound() {
+  let computerChoice = getComputerChoice();
+  let playerChoice = playerSelection();
+
+  if (playerChoice == computerChoice) {
+    return console.log(
+      "You drew!" +
+        " | " +
+        "Player Choice: " +
+        playerChoice +
+        " , " +
+        "Computer Choice: " +
+        computerChoice
+    );
+  }
+
+  if (playerChoice == "rock" && computerChoice == "scissors") {
+    return console.log(
+      "You Won!" +
+        " | " +
+        "Player Choice: " +
+        playerChoice +
+        " , " +
+        "Computer Choice: " +
+        computerChoice
+    );
+  }
+
+  if (playerChoice == "rock" && computerChoice == "paper") {
+    return console.log(
+      "You lost!" +
+        " | " +
+        "Player Choice: " +
+        playerChoice +
+        " , " +
+        "Computer Choice: " +
+        computerChoice
+    );
+  }
+
+  if (playerChoice == "paper" && computerChoice == "scissors") {
+    return console.log(
+      "You lost!" +
+        " | " +
+        "Player Choice: " +
+        playerChoice +
+        " , " +
+        "Computer Choice: " +
+        computerChoice
+    );
+  }
+
+  if (playerChoice == "paper" && computerChoice == "rock") {
+    return console.log(
+      "You Won!" +
+        " | " +
+        "Player Choice: " +
+        playerChoice +
+        " , " +
+        "Computer Choice: " +
+        computerChoice
+    );
+  }
+
+  if (playerChoice == "scissors" && computerChoice == "paper") {
+    return console.log(
+      "You Won!" +
+        " | " +
+        "Player Choice: " +
+        playerChoice +
+        " , " +
+        "Computer Choice: " +
+        computerChoice
+    );
+  }
+
+  if (playerChoice == "scissors" && computerChoice == "rock") {
+    return console.log(
+      "You Lost!" +
+        " | " +
+        "Player Choice: " +
+        playerChoice +
+        " , " +
+        "Computer Choice: " +
+        computerChoice
+    );
+  }
+}
+
+playRound();
