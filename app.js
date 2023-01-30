8cd72cc9bdc971603a251620ceea587233c06873
Martin853 <116968758+Martin853@users.noreply.github.com>
2023-01-30 20:09:34 +0100

    Added script to the index.html and created computer selection function

--- app.js
+++ app.js
@@ -2,3 +2,12 @@
 let gameOver = false;
 let playerScore = 0;
 let computerScore = 0;
+
+//Computer Selection
+const items = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+  return items[Math.floor(Math.random() * items.length)];
+}
+
+console.log(getComputerChoice());
